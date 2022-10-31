4d32654c7b955ce6f832fcdb9bd4fd953bdffc65
quent10 <quwi1951@colorado.edu>
2022-10-30 18:11:14 -0600

    tried to make it work

---
+++ assignment10/employee.js
@@ -0,0 +1,39 @@
+console.log("Employees");
+    let Sam ={
+        firstName:"Sam",
+        department:"Tech",
+        designation:"Manager",
+        salary:"40000",
+        raise:"true"
+    };
+
+    let Mary = {
+        firstName:"Mary",
+        department:"Finance",
+        designation:"Trainee",
+        salary:"18500",
+        raise:"true"
+    };
+
+    let Bill = {
+        firstName:"Bill",
+        department:"HR",
+        designation:"Executive",
+        salary:"21200",
+        raise:"false"
+    };
+    let Anna = {
+        firstName:"Anna",
+        department:"Tech",
+        designation:"Executive",
+        salary:"25600",
+        raise:"false"
+    };
+
+
+console.log("Company");
+    let company ={
+        companyName:"Tech Stars",
+        website:"www.techstars.site",
+        employees:[Sam,Mary,Bill,Anna]
+    };
