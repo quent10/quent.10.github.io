36a982e2e067bc1220c879ee11d4c22c3d1d9c29
quent10 <quwi1951@colorado.edu>
2022-12-10 11:43:05 -0700

    made video game and added a game page

--- final/main.js
+++ final/main.js
@@ -1,47 +1,111 @@
-const holes =  document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelector('.mole');
-let lastHole;
-let timeUp = false;
-let score = 0;
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
 
-function randomTime(min,max){
-    return Math.round(Math.random()*(max-min)+min);
-}
+context.scale(20,20);
 
-function randomHole(holes){
-    const idx = Math.floor(Math.random()*holes.length);
-    const hole =holes[idx];
-    if (hole === lastHole){
-        return randomHole(holes);
+
+
+const matrix = [
+    [0,0,0],
+    [1,1,1],
+    [0,1,0],
+];
+
+function collide(arena,player){
+    const[m,o] = [player.matrix,player.pos];
+    for (let y = 0; y < m.legnth; ++y){
+        for (let x = 0; x < m[y].legnth;++x){
+            if (m[y][x] !==0 &&
+                (arena[y+o.y] &&
+                arena[y+o.y][x+ o.x]) !== 0){
+                return true;  
+                }
+        }
     }
-    lastHole = hole;
-    return hole;
+    return false;
 }
 
-function peep(){
-    const time = randomTime(200, 1000);
-    const hole = randomHole(holes);
-    hole.classList.remove('up');
-    setTimeout(() =>{
-        hole.classList.remove('up');
-        if (!timeUp) peep();
-    }, time);
+function createMatrix(w,h){
+    const matreix = [];
+    while(h--){
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
 }
 
-function startGame(){
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    score = 0;
-    peep();
-    setTimeout(() => timeUp = true, 10000)
+function draw(){
+    context.fillStyle = '#000';
+    context.fillRect(0,0, canvas.width, canvas.height);
+    drawMatrix(player.matrix,player.pos);
 }
 
-function whack(e){
-    if(!e.isTrusted) return;
-    score++;
-    this.parentNode.classList.remove('up');
-    scoreBoard.textContent = score;
+
+function drawMatrix(matrix, offset){
+    matrix.forEach((row,y) => {
+        row.forEach((value,x) => {
+            if (value !== 0) {
+                context.fillStyle = 'red';
+                context.fillRect(x+ offset.x,y+offset.y,1,1);
+            }
+        });
+});
 }
 
-moles.forEach(mole => mole.addEventListener('click',whack));
+function merge(arena,player){
+    player.matrix.forEach((row,y)=>{
+        row.forEach((value,x) =>{
+            if(value!==0){
+                arena[y+player.pos.y][x+player.pos.x] = value;
+            }
+        });
+    });
+}
+
+let dropCounter = 0;
+let dropTime = 1000;
+
+let lastTime = 0;
+function update(time = 0){
+    const deltaTime = time - lastTime;
+    lastTime = time;
+
+    dropCounter += deltaTime;
+    if (dropCounter > dropTime){
+    playerDrop();
+    }
+
+    draw();
+    requestAnimationFrame(update);
+}
+const arena = createMatrix(12,20);
+
+
+function playerDrop(){
+    player.pos.y++;
+    if (collide(arena,player)){
+        player.pos.y--;
+        merge(arena,player);
+        player.pos.y = 0;
+    }
+    dropCounter = 0;
+}
+
+const player = {
+    pos: {x:5,y:5},
+    matrix: matrix,
+}
+//fix movement
+document.addEventListener('keydown', event =>{
+    console.log(event);
+    if(event.key === 'a'){
+        player.pos.x--;
+    }
+    else if(event.key ==='d'){
+        player.pos.x++;
+    }
+    else if(event.key === 's'){
+        playerDrop();
+    }
+});
+
+update();
---
+++ jumptherock/main.js
@@ -0,0 +1,35 @@
+const dino = document.getElementById('dino');
+const rock = document.getElementById('rock');
+
+function jump(){
+    dino.classList.add('jump-animation');
+    setTimeout(()=>{
+        dino.classList.remove('jump-animation');
+    }, 500);
+}
+
+document.addEventListener('keypress',()=>{
+    if (!dino.classList.contains('jump-animation')){
+        jump();
+    }
+});
+
+setInterval(()=>{
+    const dinoTop = parseInt(window.getComputedStyle(dino)
+    .getPropertyValue('top'));
+    
+    const rockLeft = parseInt(window.getComputedStyle(rock)
+    .getPropertyValue('left'));
+    
+    if (rockLeft < 0){
+        rock.style.display = 'none';
+    } 
+    else{
+        rock.style.display = '';
+    }
+
+    if(rockLeft < 50 && rockLeft > 0 && dinoTop > 150){
+        alert("GG. Try Again?");
+        location.reload();
+    }
+},50);
